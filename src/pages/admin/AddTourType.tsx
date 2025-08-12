8ae10a8c80aec55bfbac83d536e2e9a2151eb84c
mdasif8393 <md.asif.8393@gmail.com>
2025-08-12 22:07:59 +0600

    38-9 Managing Data Display with ShadCN Table

--- src/pages/admin/AddTourType.tsx
+++ src/pages/admin/AddTourType.tsx
@@ -1,5 +1,50 @@
-import React from "react";
+import { AddTourTypeModal } from "@/components/modules/Admin/TourType/AddTourTypeModal";
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { useGetTourTypesQuery } from "@/redux/features/tour/tour.api";
+import { Trash2 } from "lucide-react";
 
 export default function AddTourType() {
-  return <div>AddTourType</div>;
+  const { data } = useGetTourTypesQuery(undefined);
+
+  return (
+    <div className="w-full max-w-7xl mx-auto px-5">
+      <div className="flex justify-between my-8">
+        <h1 className="text-xl font-semibold">Tour Types</h1>
+        <AddTourTypeModal />
+      </div>
+      <div className="border border-muted rounded-md">
+        <Table>
+          <TableCaption>A list of Tour types</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Name</TableHead>
+              <TableHead className="text-right">Action</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {data?.data?.map((item: { name: string; _id: string }) => (
+              <TableRow key={item._id}>
+                <TableCell className="font-medium">{item.name}</TableCell>
+                <TableCell className="text-right">
+                  <Button size="sm">
+                    {" "}
+                    <Trash2 />{" "}
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
 }
