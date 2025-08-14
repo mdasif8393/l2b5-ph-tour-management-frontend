ffc378b0ea7c0339c5c8ce8368c70848a0d10436
mdasif8393 <md.asif.8393@gmail.com>
2025-08-14 17:09:03 +0600

    39-1 Implementing Delete Confirmation with Alert Dialog

--- src/pages/admin/AddTourType.tsx
+++ src/pages/admin/AddTourType.tsx
@@ -1,3 +1,4 @@
+import DeleteConfirmation from "@/components/DeleteConfirmation";
 import { AddTourTypeModal } from "@/components/modules/Admin/TourType/AddTourTypeModal";
 import { Button } from "@/components/ui/button";
 import {
@@ -9,11 +10,29 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { useGetTourTypesQuery } from "@/redux/features/tour/tour.api";
+import {
+  useGetTourTypesQuery,
+  useRemoveTourTypeMutation,
+} from "@/redux/features/tour/tour.api";
 import { Trash2 } from "lucide-react";
+import { toast } from "sonner";
 
 export default function AddTourType() {
   const { data } = useGetTourTypesQuery(undefined);
+
+  const [removeTourType] = useRemoveTourTypeMutation();
+
+  const handleRemoveTourType = async (tourId: string) => {
+    const toastId = toast.loading("Removing...");
+    try {
+      const res = await removeTourType(tourId).unwrap();
+      if (res.success) {
+        toast.success("", { id: toastId });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className="w-full max-w-7xl mx-auto px-5">
@@ -35,10 +54,13 @@ export default function AddTourType() {
               <TableRow key={item._id}>
                 <TableCell className="font-medium">{item.name}</TableCell>
                 <TableCell className="text-right">
-                  <Button size="sm">
-                    {" "}
-                    <Trash2 />{" "}
-                  </Button>
+                  <DeleteConfirmation
+                    onConfirm={() => handleRemoveTourType(item._id)}
+                  >
+                    <Button size="sm">
+                      <Trash2 />
+                    </Button>
+                  </DeleteConfirmation>
                 </TableCell>
               </TableRow>
             ))}
--- src/redux/features/tour/tour.api.ts
+++ src/redux/features/tour/tour.api.ts
@@ -10,6 +10,13 @@ export const tourApi = baseApi.injectEndpoints({
       }),
       invalidatesTags: ["TOUR"],
     }),
+    removeTourType: builder.mutation({
+      query: (tourTypeId) => ({
+        url: `/tour/tour-types/${tourTypeId}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["TOUR"],
+    }),
 
     getTourTypes: builder.query({
       query: () => ({
@@ -22,4 +29,8 @@ export const tourApi = baseApi.injectEndpoints({
   }),
 });
 
-export const { useGetTourTypesQuery, useAddTourTypeMutation } = tourApi;
+export const {
+  useGetTourTypesQuery,
+  useAddTourTypeMutation,
+  useRemoveTourTypeMutation,
+} = tourApi;
